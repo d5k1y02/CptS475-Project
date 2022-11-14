29c8978f29a9b8d159a3091aec84908508c81405
d5k1y02 <d5k1y02@gmail.com>
2022-11-14 11:24:16 -0800

    added project directives

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1. read and load the dataset into memory
+
+
+# 2. convert the dataset into a format suitable for learning
+
+
+# 3. select month of data and split the data into 
+# the train set and testing set
+
+
+# 4. create the classifier
+
+# 5. train the classifyier
+
+# 6. display results
