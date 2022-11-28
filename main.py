9327df4f31de0a798b5a5e08830618a987244b35
SebSmith1357 <7hunter.sebastian@gmail.com>
2022-11-28 09:09:22 -0800

    Added random index-generating functionality

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from numpy import load
 from skmultiflow.data.file_stream import FileStream
 from skmultiflow.data.data_stream import DataStream
 from datetime import datetime
+from numpy import loadtxt
+import random
+import math
 import csv
 # 1. read and load the dataset into memory
 stream = FileStream("bodmas.csv")
@@ -29,6 +32,15 @@ with open('bodmas_metadata.csv', newline='') as csvfile:
             month_count = 1
 
 print(month_counts)
+
+with open('bodmas_metadata.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    starting_index = 0
+    for value in month_counts:
+        print(type(month_counts[value]))
+#        print(random.randint(value, size = math.ceil(month_counts[value] * 0.75)))
+#        print(random.randint(month_counts[value], size = math.ceil(month_counts[value] * 0.75)))
+        print(random.sample(range(0, month_counts[value]), math.ceil(month_counts[value] * 0.75)))
 
 # 2. convert the dataset into a format suitable for learning
 
