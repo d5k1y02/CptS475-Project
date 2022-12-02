10b0dc9e455abc49506b39979163990d7cfbedf6
SebSmith1357 <7hunter.sebastian@gmail.com>
2022-12-02 10:20:44 -0800

    Added classifier-saving and loading functionality and iteration through months of the dataset

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-
-from skmultiflow.data.data_stream import DataStream
+from skmultiflow.data.file_stream import FileStream
+from skmultiflow.meta import AdaptiveRandomForestClassifier
 from datetime import datetime
 from numpy import loadtxt
 import random
 import math
 import csv
+import pickle
 # 1. read and load the dataset into memory
 stream = FileStream("bodmas.csv")
-print(stream.next_sample())
-print(type(stream))
 
 month_counts = {}
 month_count = 0 
@@ -30,6 +29,34 @@ with open('bodmas_metadata.csv', newline='') as csvfile:
             current_month = str(datetime_object.year) + ' ' + str(datetime_object.month)
             month_count = 1
 
+print("month counts: ")
 print(month_counts)
 
+# 2. create and train the classifier
+classifier = AdaptiveRandomForestClassifier(n_estimators = 1, max_features = 10)
 
+n_samples = 0
+correct_cnt = 0
+#samples = month_counts #dictionary of the number of samples in each month
+
+for value in month_counts:
+    print(month_counts[value])
+    n_samples = 0
+    correct_cnt = 0
+    while n_samples < month_counts[value] and stream.has_more_samples():
+        X, y = stream.next_sample()
+        y_pred = classifier.predict(X)
+        if y[0] == y_pred[0]:
+            correct_cnt += 1
+        classifier.partial_fit(X, y)
+        n_samples += 1
+
+    print('{} samples analyzed.'.format(n_samples)) #Display results
+    print('Accuracy: {}'.format(correct_cnt / n_samples))
+
+    pickle.dump(classifier, open("classifier.sav", 'wb'))
+    classifier = pickle.load(open("classifier.sav", 'rb'))
+
+# 3. display results
+print('{} samples analyzed.'.format(n_samples))
+print('Accuracy: {}'.format(correct_cnt / n_samples))
