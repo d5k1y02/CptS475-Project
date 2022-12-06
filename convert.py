baa8c6ad26b64773398006e1b5e83d0e96922f0e
d5k1y02 <d5k1y02@gmail.com>
2022-12-06 15:56:28 -0800

    moved y label to the end of each list

--- convert.py
+++ convert.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 data = np.load('bodmas.npz')
 frame = pd.DataFrame(data['X'])
-frame.insert(0, 'class', data['y'])
+frame.insert(2381, 'class', data['y'])
 frame.to_csv("bodmas.csv")
